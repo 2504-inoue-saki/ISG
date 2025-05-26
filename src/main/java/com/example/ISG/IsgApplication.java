ab1902ec433481f9f9af38d363a88a2b386bf5e8
inoue_saki <inoue.saki@alhinc.jp>
2025-05-26 11:38:48 +0900

    First Commit

---
+++ src/main/java/com/example/ISG/IsgApplication.java
@@ -0,0 +1,13 @@
+package com.example.ISG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ISG/IsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ISG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
