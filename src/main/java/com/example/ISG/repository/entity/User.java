4c825aa2361787ee50ee159276f34ae54423ec47
ganaha_mikoto <ganaha.mikoto@alhinc.jp>
2025-05-26 15:02:53 +0900

    lombokの導入とUserEntityの途中

--- src/main/java/com/example/ISG/repository/entity/User.java
+++ src/main/java/com/example/ISG/repository/entity/User.java
@@ -1,4 +1,30 @@
 package com.example.ISG.repository.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "User")
+@Getter
+@Setter
 public class User {
+@id
+private Integer id;
+
+@Column
+    private String account;
+    private String password;
+    private String name;
+    private Integer branchId;
+    private String account;
+    private String account;
+    private String account;
+
 }
--- src/main/java/com/example/ISG/repository/entity/UserMessage.java
+++ src/main/java/com/example/ISG/repository/entity/UserMessage.java
@@ -1,4 +1,12 @@
 package com.example.ISG.repository.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
 
+@Getter
+@Setter
 public class UserMessage {
+
 }
