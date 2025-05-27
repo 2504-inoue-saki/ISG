2fd532ca15919d27f84a9c00807b0d110149ea8d
ganaha_mikoto <ganaha.mikoto@alhinc.jp>
2025-05-27 09:47:41 +0900

    メッセージ表示機能

--- src/main/java/com/example/ISG/controller/TopController.java
+++ src/main/java/com/example/ISG/controller/TopController.java
@@ -3,6 +3,8 @@ package com.example.ISG.controller;
 import com.example.ISG.controller.form.CommentForm;
 import com.example.ISG.controller.form.MessageForm;
 import com.example.ISG.repository.entity.Message;
+import com.example.ISG.service.CommentService;
+import com.example.ISG.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,20 +29,18 @@ public class TopController {
         ModelAndView mav = new ModelAndView();
         // 投稿を全件取得した値を入れる箱（contentData）をつくってサービスに渡しています
         List<MessageForm> contentData = messageService.findAllMessage(start ,end);
-        List<CommentForm> commentData = commentService.findAllComment();
+//        List<CommentForm> commentData = commentService.findAllComment();
 
         // 画面遷移先を指定 「現在のURL」/top へ画面遷移することを指定します。
         mav.setViewName("/top");
         // 投稿データオブジェクトを先ほどのcontentDataをModelAndView型の変数mavへ格納します。
         mav.addObject("contents", contentData);
-        mav.addObject("comments", commentData);
+//        mav.addObject("comments", commentData);
         mav.addObject("start",start);
         mav.addObject("end", end);
 
 
         /* 変数mavを戻り値として返します。 */
         return mav;
-
-        return null;
     }
 }
