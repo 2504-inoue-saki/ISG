23c814f7529866b2a500260d3c0d52510d12a62e
inoue_saki <inoue.saki@alhinc.jp>
2025-05-29 10:46:22 +0900

    ユーザ管理

--- src/main/java/com/example/ISG/controller/UserController.java
+++ src/main/java/com/example/ISG/controller/UserController.java
@@ -7,6 +7,7 @@ import com.example.ISG.dto.UserBranchDepartment;
 import com.example.ISG.service.UserService;
 import io.micrometer.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+@Controller
 public class UserController {
 @Autowired UserService userService;
     /*
--- src/main/resources/static/js/updateIsStopped.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('.update-isStopped-button').on('click', function() {
-		if (confirm($(this).parents('.update-isStopped').find('input[name="userName"]').val() + "を" + $(this).parents('.update-isStopped').find('option:selected').text() + "状態にします。よろしいですか？")) {
-			return true;
-		}
-		return false;
-	});
-});
